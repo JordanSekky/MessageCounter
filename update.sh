89aeb7a1cb410f611ac87b298231e3098fe29768
Jordan Sechler <jordan.sechler@gmail.com>
2015-01-01 19:31:12 -0500

    Added bash script for downloading the database files from a jailbroken iphone through ssh.

---
+++ update.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+echo Please, enter your name
+read NAME
+echo What is the ip of the jailbroken phone?
+read IP
+mkdir -p $NAME
+cd $NAME
+mkdir -p SMS
+mkdir -p AddressBook
+cd SMS
+sftp root@$IP:/var/mobile/Library/SMS/sms.db*
+cd ../AddressBook
+sftp root@$IP:/var/mobile/Library/AddressBook/AddressBook.sqlitedb*
